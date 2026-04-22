97149adf95cde85d5748644f48eceb749ec7674f
Dario Cipolletta <dario.cipolletta.0711@gmail.com>
2026-04-22 18:15:27 +0200

    feat: add FizzBuzz solution

---
+++ FizzBuzz.java
@@ -0,0 +1,36 @@
+public class FizzBuzz
+{
+	static void main(String[] args)
+	{
+		fizzbuzz(100);
+	}
+	
+	public static void fizzbuzz(int number)
+	{
+		for (int i = 1; number >= i; i = i + 1)
+		{
+	
+			if (i % 3 == 0)
+			{
+				if (i % 5 == 0)
+				{
+					System.out.println(i + " FizzBuzz");
+				}
+				else
+				{
+					System.out.println(i + " Fizz");
+				}
+			}
+			else if(i%5 == 0){
+				System.out.println(i + " Buzz");
+				
+			}
+			else {
+				System.out.println(i);
+			}
+			
+		}
+	}
+	
+	
+}
