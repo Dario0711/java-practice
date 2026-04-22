451d604baa3c92cad6f276b24b9e621f0bba9710
Dario Cipolletta <dario.cipolletta.0711@gmail.com>
2026-04-22 18:29:30 +0200

    Feat: add TemperatureConverter solution

---
+++ TemperatureConverter.java
@@ -0,0 +1,38 @@
+
+import java.util.InputMismatchException;
+import java.util.Scanner;  // Import the Scanner class
+
+public class TemperatureConverter
+{
+	
+	static void main()
+	{
+		ConvertTemp();
+	}
+	
+	
+	public static void ConvertTemp()
+	{
+		Scanner myScanner = new Scanner(System.in);
+		
+		System.out.println("Enter Temperature in Celsius");
+		
+		do
+		{
+			try
+			{
+				double temperatureInCelsius = myScanner.nextDouble();
+				
+				double convertedTemperatureInFahrenheit = (temperatureInCelsius * (9.0 / 5)) + 32;
+				
+				System.out.println("Temperature in Fahrenheit is: " + convertedTemperatureInFahrenheit);
+				
+			} catch (InputMismatchException e)
+			{
+				System.out.println("Only Numbers are valid");
+				myScanner.next();
+			}
+		}
+		while (true);
+	}
+}
