de0cb14c9137e52659ad6f1e79f4615270c96bbf
Dario Cipolletta <dario.cipolletta.0711@gmail.com>
2026-04-23 20:53:18 +0200

    feat: added logic to getArrayStats

--- src/ArrayStats.java
+++ src/ArrayStats.java
@@ -1,6 +1,26 @@
 public class ArrayStats {
 	public static void main(String[] args) {
-	
-	
+		int[] arr = {10, 20, 22, 41, 5, 11, 25, 30, 40, 91};
+		getArrayStats(arr);
+	}
+	public static void getArrayStats(int[] array) {
+		int highestNumber = array[0];
+		int lowestNumber = array[0];
+		int arraySum = 0;
+		double arrayAverage = 0.0;
+		for (int i = 0; i < array.length; i++) {
+			arraySum += array[i];
+			if (highestNumber < array[i]) {
+				highestNumber = array[i];
+			}
+			else if (lowestNumber > array[i]) {
+				lowestNumber = array[i];
+			}
+		}
+		System.out.println(highestNumber);
+		System.out.println(lowestNumber);
+		System.out.println(arraySum);
+		arrayAverage += arraySum;
+		System.out.println(arrayAverage / array.length);
 	}
 }
