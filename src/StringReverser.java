b8d30fe3a56a5a7fa6ba9bab5adc0caafcf30746
Dario Cipolletta <dario.cipolletta.0711@gmail.com>
2026-04-23 21:05:48 +0200

    feat: created and completed reverseString function

--- src/StringReverser.java
+++ src/StringReverser.java
@@ -1,4 +1,28 @@
+import java.util.Scanner;
+
 public class StringReverser {
 	public static void main(String[] args) {
+		reverseString();
+	}
+	public static void reverseString() {
+		Scanner myScanner = new Scanner(System.in);
+		System.out.println("Enter String you want to reverse (or ':quit' to exit)");
+		while (true) {
+			String input = myScanner.nextLine();
+			String output = "";
+			if (input.equalsIgnoreCase(":quit")) {
+				System.out.println("Goodbye!");
+				break;
+			}
+			try {
+				for (int i = input.length() - 1; i >= 0; i--) {
+					output += input.charAt(i);
+					System.out.println(input.charAt(i));
+				}
+			} catch (NumberFormatException e) {
+				System.out.println("Only numbers or 'quit' are valid");
+			}
+			System.out.println(output);
+		}
 	}
 }
