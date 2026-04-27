d125466b50b57c0ffc5813dc6e9ca684d7dd3caf
Dario Cipolletta <dario.cipolletta.0711@gmail.com>
2026-04-27 19:54:29 +0200

    feat: added list all books in bookshelf function

--- src/classTasks/Bookshelf.java
+++ src/classTasks/Bookshelf.java
@@ -11,6 +11,19 @@ public class Bookshelf {
 	
 	}
 	
+	public static void main(String[] args) {
+		Bookshelf bookshelf = new Bookshelf();
+		Book lotr = new Book("Lord of the Rings", "J.R.R. Tolkien", 510);
+		Book hp = new Book("Harry Potter", "J.K. Rowling", 322);
+		Book zuffenhausen = new Book("Zuffenhausen", "Dario Cipolletta", 1889);
+		
+		bookshelf.addBookToBookshelf(lotr);
+		bookshelf.addBookToBookshelf(hp);
+		bookshelf.addBookToBookshelf(zuffenhausen);
+		
+		System.out.println(bookshelf.listAllBooks());
+	}
+	
 	public void addBookToBookshelf(Book book) {
 		// if book is already in booklsit then do nothing
 		if (!bookList.contains(book)) {
@@ -22,12 +35,18 @@ public class Bookshelf {
 		}
 	}
 	
-	public static void main(String[] args) {
-		Bookshelf bookshelf = new Bookshelf();
-		Book lotr = new Book("Me", "you", 22);
+	public String listAllBooks() {
+		String allBooks = "";
 		
-		bookshelf.addBookToBookshelf(lotr);
-		bookshelf.addBookToBookshelf(lotr);
-		
+		for (int i = 0; i < bookList.size(); i++) {
+			if (allBooks == "") {
+				allBooks += bookList.get(i).title;
+			}
+			else {
+				allBooks += ", " + bookList.get(i).title;
+				
+			}
+		}
+		return allBooks;
 	}
 }
